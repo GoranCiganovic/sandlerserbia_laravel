67ab331f5c7c0a027c6ad2163850288a41c501c4
Goran <goranciganovic23@gmail.com>
2018-08-13 09:47:52 +0200

    Update

--- sandlerserbialaravel_app/app/Article.php
+++ sandlerserbialaravel_app/app/Article.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App;
-
+ 
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
@@ -351,6 +351,7 @@ class Article extends Model
         $article['middle_articles'] = $this->get_middle_articles_html();
         $article['payings'] = $this->get_payings_html();
         $article['bottom_articles'] = $this->get_bottom_articles_html();
+		
         return $article;
     }
 }
